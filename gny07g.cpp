ddde5aebbc54e379d90b16dee2d2622420ecdf14
Brian Bi <bbi5291@gmail.com>
2023-10-20 21:33:33 -0400

    fix GNY07G

--- gny07g.cpp
+++ gny07g.cpp
@@ -1,11 +1,11 @@
-// 2008-10-28
-#include <iostream>
-#include <cmath>
+// 2023-10-20
+// originally written 2008-10-28 but with compilation errors
+#include <math.h>
+#include <stdio.h>
 #define Sin(x) (sin((x)/180.0*3.1415926536))
 #define Cos(x) (cos((x)/180.0*3.1415926536))
 #define Tan(x) (Sin(x)/Cos(x))
 #define Abs(x) ((x)>0?(x):-(x))
-using namespace std;
 int main()
 {
 	int N,i;
@@ -20,17 +20,6 @@ int main()
 			printf("%d DISQUALIFIED\n",i);
 			continue;
 		}
-		x=D*Cos(c)*Sin(d)/Sin(d-c);
-		y=D*Sin(c)*Sin(d)/Sin(d-c);
-		Z_a=sqrt(x*x+y*y)*Tan(a)+HA;
-		Z_b=sqrt((D-x)*(D-x)+y*y)*Tan(b)+HB;
-		if (Abs(Z_a-Z_b)>ERRDIST)
-			printf("%d ERROR\n",i);
-		else
-			printf("%d %.0f\n",i,(Z_a+Z_b)/2.0);
-	}
-	return 0;
-}
 		//plane 1: vertical (z-value arbitrary),
 		//passes through (0,0) and (cos c, sin c)
 		//so equation is (sin c)x - (cos c)y = 0
@@ -46,8 +35,19 @@ int main()
 		//y = (D sin c sin d)/(cos c sin d - sin c cos d)
 		/*y = (D sin c sin d)/sin(d - c)
 		  x = (D cos c sin d)/sin(d - c)*/
+		x=D*Cos(c)*Sin(d)/Sin(d-c);
+		y=D*Sin(c)*Sin(d)/Sin(d-c);
 		//and for the line from A, the elevation angle is a
 		//so we define a plane... with satisfies z/sqrt(x^2+y^2) = tan a
 		//or z = sqrt(x^2+y^2) tan a
 		//and for the line from B, the elevation angle is b
 		//another plane... here z/sqrt((D-x)^2+y^2) = tan b
+		Z_a=sqrt(x*x+y*y)*Tan(a)+HA;
+		Z_b=sqrt((D-x)*(D-x)+y*y)*Tan(b)+HB;
+		if (Abs(Z_a-Z_b)>ERRDIST)
+			printf("%d ERROR\n",i);
+		else
+			printf("%d %.0f\n",i,(Z_a+Z_b)/2.0);
+	}
+	return 0;
+}
