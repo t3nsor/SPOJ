6ef91466869148a011273c90dfb88761a1c7254d
Brian Bi <bbi5291@gmail.com>
2023-12-23 00:31:18 -0500

    use my own max flow for POTHOLE

--- pothole.cpp
+++ pothole.cpp
@@ -1,100 +1,107 @@
-// 2014-06-07
-#include <vector>
-#include <cstdio>
 #include <algorithm>
+#include <stdio.h>
+#include <utility>
+#include <vector>
 using namespace std;
-// The following code for Dinic's algorithm is copied from Stanford's ACM team
-// notebook, the latest version of which may be found at:
-// http://web.stanford.edu/~liszt90/acm/notebook_official.pdf
-const int INF = 2000000000;
-
-struct Edge {
-  int from, to, cap, flow, index;
-  Edge(int from, int to, int cap, int flow, int index) :
-    from(from), to(to), cap(cap), flow(flow), index(index) {}
+struct st {
+    int v;
+    int cap;
+    int back;
 };
-
-struct Dinic {
-  int N;
-  vector<vector<Edge> > G;
-  vector<Edge *> dad;
-  vector<int> Q;
-  
-  // N = number of vertices
-  Dinic(int N) : N(N), G(N), dad(N), Q(N) {}
-  
-  // Add an edge to initially empty network. from, to are 0-based
-  void AddEdge(int from, int to, int cap) {
-    G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
-    if (from == to) G[from].back().index++;
-    G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
-  }
-
-  long long BlockingFlow(int s, int t) {
-    fill(dad.begin(), dad.end(), (Edge *) NULL);
-    dad[s] = &G[0][0] - 1;
-    
-    int head = 0, tail = 0;
-    Q[tail++] = s;
-    while (head < tail) {
-      int x = Q[head++];
-      for (int i = 0; i < G[x].size(); i++) {
-        Edge &e = G[x][i];
-        if (!dad[e.to] && e.cap - e.flow > 0) {
-          dad[e.to] = &G[x][i];
-          Q[tail++] = e.to;
+// Brian's implementation of the relabel-to-front algorithm with gap heuristic.
+int get_max_flow(vector<vector<st>>&& adj, int s, int t) {
+    struct ListNode {
+        int prev;
+        int next;
+    };
+    const int V = adj.size();
+    vector<int> excess(V, 0);
+    vector<int> height(V, 0);
+    vector<int> heightcnt(2*V + 1, 0);
+    vector<ListNode> L(V + 1);
+    L[V].prev = V;
+    L[V].next = V;
+    for (int i = 0; i < V; i++) {
+        if (i == s || i == t) continue;
+        L[i].next = V;
+        L[i].prev = L[V].prev;
+        L[L[V].prev].next = i;
+        L[V].prev = i;
+    }
+    height[s] = V;
+    heightcnt[V] = 1;
+    heightcnt[0] = V - 1;
+    for (auto& edge : adj[s]) {
+        excess[edge.v] += edge.cap;
+        adj[edge.v][edge.back].cap = edge.cap;
+        edge.cap = 0;
+    }
+    int u = L[V].next;
+    while (u != V) {
+        bool relabelled = false;
+        while (excess[u] > 0) {
+            int best = 1e9;
+            for (auto& edge : adj[u]) {
+                if (!edge.cap) continue;
+                if (height[u] == height[edge.v] + 1) {
+                    const int x = min(excess[u], edge.cap);
+                    excess[u] -= x;
+                    excess[edge.v] += x;
+                    edge.cap -= x;
+                    adj[edge.v][edge.back].cap += x;
+                    if (excess[u] == 0) break;
+                } else {
+                    best = min(best, height[edge.v]);
+                }
+            }
+            if (excess[u] > 0) {
+                relabelled = true;
+                const int old_height = height[u];
+                height[u] = best + 1;
+                heightcnt[best + 1]++;
+                if (0 == --heightcnt[old_height]) {
+                    for (int i = 0; i < V; i++) {
+                        if (height[i] > old_height && height[i] <= V) {
+                            height[i] = V + 1;
+                        }
+                    }
+                }
+            }
         }
-      }
+        if (relabelled) {
+            L[L[u].prev].next = L[u].next;
+            L[L[u].next].prev = L[u].prev;
+            L[u].next = L[V].next;
+            L[u].prev = V;
+            L[L[V].next].prev = u;
+            L[V].next = u;
+        } else {
+            u = L[u].next;
+        }
     }
-    if (!dad[t]) return 0;
+    return excess[t];
+}
 
-    long long totflow = 0;
-    for (int i = 0; i < G[t].size(); i++) {
-      Edge *start = &G[G[t][i].to][G[t][i].index];
-      int amt = INF;
-      for (Edge *e = start; amt && e != dad[s]; e = dad[e->from]) {
-        if (!e) { amt = 0; break; }
-        amt = min(amt, e->cap - e->flow);
-      }
-      if (amt == 0) continue;
-      for (Edge *e = start; amt && e != dad[s]; e = dad[e->from]) {
-        e->flow += amt;
-        G[e->to][e->index].flow -= amt;
-      }
-      totflow += amt;
-    }
-    return totflow;
-  }
-
-  // Call this to get the max flow. s, t are 0-based.
-  // Note, you can only call this once.
-  // To obtain the actual flow values, look at all edges with
-  // capacity > 0 (zero capacity edges are residual edges).
-
-  long long GetMaxFlow(int s, int t) {
-    long long totflow = 0;
-    while (long long flow = BlockingFlow(s, t))
-      totflow += flow;
-    return totflow;
-  }
-};
+const int INF = 1000000000;
 
 int main() {
     int t; scanf("%d", &t);
     while (t--) {
         int n; scanf("%d", &n);
-        Dinic D(n);
+        vector<vector<st>> adj(n);
         for (int i = 0; i < n-1; i++) {
             int m; scanf("%d", &m);
             while (m--) {
                 int x; scanf("%d", &x); x--;
                 if (i == 0 || x == n-1) {
-                    D.AddEdge(i, x, 1);
+                    adj[i].push_back(st{x, 1, (int)adj[x].size()});
+                    adj[x].push_back(st{i, 0, (int)adj[i].size() - 1});
                 } else {
-                    D.AddEdge(i, x, INF);
+                    adj[i].push_back(st{x, INF, (int)adj[x].size()});
+                    adj[x].push_back(st{i, 0, (int)adj[i].size() - 1});
                 }
             }
         }
-        printf("%d\n", D.GetMaxFlow(0, n-1));
+        printf("%d\n", get_max_flow(move(adj), 0, n - 1));
     }
 }
