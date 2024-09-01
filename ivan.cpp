529c308f816d11aabf34fed18f04686eb9e321c0
Brian Bi <bbi5291@gmail.com>
2024-08-31 21:40:54 -0400

    simplify IVAN

--- ivan.cpp
+++ ivan.cpp
@@ -3,9 +3,18 @@
 // S1 * S2. Then, the basic observation is that an optimal move never takes more
 // than 1 number from both arrays, because taking K1 >= 2 and K2 >= 2 numbers
 // would always cost more than a sequence of two moves consisting of taking 1
-// number from both arrays and then taking K1 - 1 and K2 - 1. This gives an
-// O(L^3) algorithm, and by doing just a bit of algebra (explained below) we can
-// optimize the transition to O(L^2) which will pass.
+// number from both arrays and then taking K1 - 1 and K2 - 1. Naively trying all
+// possible transitions gives an O(L^3) algorithm.
+//
+// Let dp[i][j] be the minimum cost starting from A[0...i-1] and B[0...j-1].
+// Suppose we were to take 1 element from the former and K2 >= 2 elements from
+// the latter. The cost of this would be the same as the cost of taking 1
+// element from A[0...i-1] and K2 - 1 elements from B[0...j-2] and then adding
+// A[i-1]*B[j-1]. But all possible values for K2 are already included in the
+// computation of dp[i][j-1], so instead of iterating over all values of K2 >= 2
+// we just take dp[i][j-1] + A[i-1]*B[j-1]. Similarly instead of iterating over
+// all values of K1 >= 2 we just take dp[i-1][j] + A[i-1]*B[j-1]. And finally we
+// take into account the case of K1 = K2 = 1.
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -16,30 +25,13 @@ int main() {
     for (int i = 0; i < M; i++) { cin >> A[i]; A[i]--; }
     for (int j = 0; j < N; j++) { cin >> B[j]; B[j]--; }
     vector<vector<long long>> dp(M + 1, vector<long long>(N + 1, 3e9));
-    dp[1][1] = A[0] * B[0];
-    for (int j = 2; j <= N; j++) {
-        dp[1][j] = dp[1][j - 1] + A[0] * B[j - 1];
-    }
-    vector<long long> C(N + 1, 3e9);
-    for (int i = 2; i <= M; i++) {
-        dp[i][1] = dp[i - 1][1] + A[i - 1] * B[0];
-        long long D = 3e9;
-        for (int j = 2; j <= N; j++) {
-            // To determine
-            // min_{1 <= i' < i} (dp[i'][j - 1] +
-            //                    B[j - 1] sum_{i' <= i'' < i} A[i''])
-            // we call this quantity C[i][j] and observe that
-            // C[i][j] = min(C[i - 1][j] + A[i - 1] B[j - 1],
-            //               dp[i - 1][j - 1] + A[i - 1] B[j - 1])
-            C[j] = min(C[j], dp[i - 1][j - 1]) + A[i - 1] * B[j - 1];
-            // Similarly, to determine
-            // min_{1 <= j' < j} (dp[i - 1][j'] +
-            //                    A[i - 1] sum_{j' <= j'' < j} B[j''])
-            // we call this quantity D[i][j] and observe that
-            // D[i][j] = min(D[i][j - 1] + A[i - 1] B[j - 1],
-            //               dp[i - 1][j - 1] + A[i - 1] B[j - 1])
-            D = min(D, dp[i - 1][j - 1]) + A[i - 1] * B[j - 1];
-            dp[i][j] = min(C[j], D);
+    dp[0][0] = 0;
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            dp[i][j] = dp[i - 1][j - 1];
+            if (i > 1) dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+            if (j > 1) dp[i][j] = min(dp[i][j], dp[i][j - 1]);
+            dp[i][j] += A[i - 1] * B[j - 1];
         }
     }
     cout << dp[M][N] << '\n';
