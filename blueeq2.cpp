878af34f6b666ca15ee8ef27032aa6f3362a48cd
Brian Bi <bbi5291@gmail.com>
2024-10-29 19:43:03 -0400

    remove unnecessary templates from BLUEEQ2

--- blueeq2.cpp
+++ blueeq2.cpp
@@ -1,11 +1,10 @@
 // 2024-10-29
 // This is a brute force problem; we must optimize the meet-in-the-middle
-// approach enough to get within the time limit. This solution uses templates to
-// minimize the number of indirect loads. That's probably overkill though; I
-// imagine a solution that only uses ordinary functions would be almost as fast.
-// We use radix sort, which takes linear time. (The tricky part is how to get
-// the negative numbers ordered before the positive ones; this took some tries
-// to get right.)
+// approach enough to get within the time limit. So we use radix sort, which
+// takes linear time. (The tricky part is how to get the negative numbers
+// ordered before the positive ones; this took some tries to get right.) And
+// of course a two-pointer walk over the sorted lists, instead of binary search
+// or whatever.
 #include <iostream>
 #include <limits.h>
 #include <string.h>
@@ -14,47 +13,38 @@ unsigned pwr[151][31];
 unsigned a[3375000], b[3375000];
 unsigned k[6], p[6];
 unsigned m;
-template <unsigned* dest, int start, int len>
-auto gen = []{};
 
-template <unsigned* dest, int start>
-auto gen<dest, start, 1> = [] {
+void gen(unsigned* dest, int start, int len) {
+    if (len == 0) return;
     unsigned* cur = dest;
-    for (unsigned x0 = 1; x0 <= m; x0++) {
-        *cur++ = k[start] * pwr[x0][p[start]];
-    }
-};
-
-template <unsigned* dest, int start>
-auto gen<dest, start, 2> = [] {
-    unsigned* cur = dest;
-    for (unsigned x0 = 1; x0 <= m; x0++) {
-        unsigned y = k[start] * pwr[x0][p[start]];
-        for (unsigned x1 = 1; x1 <= m; x1++) {
-            *cur++ = y + k[start + 1] * pwr[x1][p[start + 1]];
+    if (len == 1) {
+        for (unsigned x0 = 1; x0 <= m; x0++) {
+            *cur++ = k[start] * pwr[x0][p[start]];
         }
-    }
-};
-
-template <unsigned* dest, int start>
-auto gen<dest, start, 3> = [] {
-    unsigned* cur = dest;
-    for (unsigned x0 = 1; x0 <= m; x0++) {
-        unsigned y = k[start] * pwr[x0][p[start]];
-        for (unsigned x1 = 1; x1 <= m; x1++) {
-            unsigned z = y + k[start + 1] * pwr[x1][p[start + 1]];
-            for (unsigned x2 = 1; x2 <= m; x2++) {
-                *cur++ = z + k[start + 2] * pwr[x2][p[start + 2]];
+    } else if (len == 2) {
+        for (unsigned x0 = 1; x0 <= m; x0++) {
+            unsigned y = k[start] * pwr[x0][p[start]];
+            for (unsigned x1 = 1; x1 <= m; x1++) {
+                *cur++ = y + k[start + 1] * pwr[x1][p[start + 1]];
+            }
+        }
+    } else {
+        for (unsigned x0 = 1; x0 <= m; x0++) {
+            unsigned y = k[start] * pwr[x0][p[start]];
+            for (unsigned x1 = 1; x1 <= m; x1++) {
+                unsigned z = y + k[start + 1] * pwr[x1][p[start + 1]];
+                for (unsigned x2 = 1; x2 <= m; x2++) {
+                    *cur++ = z + k[start + 2] * pwr[x2][p[start + 2]];
+                }
             }
         }
     }
-};
+}
 
 unsigned buf[3375000];
 int ofs[65536];
 
-template <unsigned* a>
-void radixsort(int len) {
+void radixsort(unsigned* a, int len) {
     memset(ofs, 0, sizeof(ofs));
     for (int i = 0; i < len; i++) {
         ++ofs[(a[i] + 1) & 0xffff];
@@ -97,32 +87,12 @@ void do_testcase() {
         cout << (int)(ksum == 0U) << '\n';
         return;
     }
+    gen(a, 0, n/2);
+    gen(b, n/2, (n + 1)/2);
     const int alen = pwr[m][n/2];
     const int blen = pwr[m][(n + 1)/2];
-    if (n == 0) {
-        gen<a, 0, 0>();
-        gen<b, 0, 0>();
-    } else if (n == 1) {
-        gen<a, 0, 0>();
-        gen<b, 0, 1>();
-    } else if (n == 2) {
-        gen<a, 0, 1>();
-        gen<b, 1, 1>();
-    } else if (n == 3) {
-        gen<a, 0, 1>();
-        gen<b, 1, 2>();
-    } else if (n == 4) {
-        gen<a, 0, 2>();
-        gen<b, 2, 2>();
-    } else if (n == 5) {
-        gen<a, 0, 2>();
-        gen<b, 2, 3>();
-    } else {
-        gen<a, 0, 3>();
-        gen<b, 3, 3>();
-    }
-    radixsort<a>(alen);
-    radixsort<b>(blen);
+    radixsort(a, alen);
+    radixsort(b, blen);
     int result = 0;
     int s = blen - 1, i = 0, j = 0;
     while (s >= 0) {
