33a698c04b24faafe7d361da7a560a1282436fc2
Brian Bi <bbi5291@gmail.com>
2026-04-25 02:06:34 -0400

    add CAKE2

---
+++ cake2.cpp
@@ -0,0 +1,44 @@
+// 2026-04-25
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+#include <utility>
+#include <vector>
+using namespace std;
+long long gcd(long long x, long long y) {
+    return x == 0 ? y : gcd(y % x, x);
+}
+int main() {
+    int N; cin >> N;
+    vector<long long> X1(N), Y1(N), X2(N), Y2(N);
+    int result = 1;
+    for (int i = 0; i < N; i++) {
+        cin >> X1[i] >> Y1[i] >> X2[i] >> Y2[i];
+        const long long x1 = X1[i];
+        const long long y1 = Y1[i];
+        const long long x2 = X2[i];
+        const long long y2 = Y2[i];
+        vector<pair<long long, long long>> isct;
+        for (int j = 0; j < i; j++) {
+            const long long x3 = X1[j];
+            const long long y3 = Y1[j];
+            const long long x4 = X2[j];
+            const long long y4 = Y2[j];
+            long long den = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
+            if (den == 0) continue;
+            long long num = (x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4);
+            const long long g = gcd(abs(num), abs(den));
+            num /= g;
+            den /= g;
+            if (den < 0) { num = -num; den = -den; }
+            isct.emplace_back(num, den);
+        }
+        sort(isct.begin(), isct.end());
+        int ucnt = 0;
+        for (int j = 0; j < isct.size(); j++) {
+            if (j == 0 || isct[j] != isct[j - 1]) ++ucnt;
+        }
+        result += ucnt + 1;
+    }
+    cout << result << '\n';
+}
